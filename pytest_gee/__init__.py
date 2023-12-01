24811fd7550fd193d24ca5c6d392c099a413ea75
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-12-01 16:43:47 +0000

    feat: add 2 first plugin content

--- pytest_gee/__init__.py
+++ pytest_gee/__init__.py
@@ -1,20 +1,37 @@
 """The init file of the package."""
+import os
+from pathlib import Path
+
+import ee
+import httplib2
 
 __version__ = "0.0.0"
 __author__ = "Pierrick Rambaud"
 __email__ = "pierrick.rambaud49@gmail.com"
 
 
-class Hello:
-    """Hello world class."""
+def init_ee_from_token():
+    r"""Initialize earth engine according to the using a token.
 
-    msg = "hello world !"
-    "the message to print"
+    THe environment used to run the tests need to have a EARTHENGINE_TOKEN variable.
+    The content of this variable must be the copy of a personal credential file that you can find on your local computer if you already run the earth engine command line tool. See the usage question for a github action example.
 
-    def hello_world(self) -> str:
-        """Hello world demo method.
+    - Windows: ``C:\Users\USERNAME\\.config\\earthengine\\credentials``
+    - Linux: ``/home/USERNAME/.config/earthengine/credentials``
+    - MacOS: ``/Users/USERNAME/.config/earthengine/credentials``
 
-        Returns:
-            the hello world string
-        """
-        return self.msg
+    Note:
+        As all init method of pytest-gee, this method will fallback to a regular ``ee.Initialize()`` if the environment variable is not found e.g. on your local computer.
+    """
+    if "EARTHENGINE_TOKEN" in os.environ:
+
+        # write the token to the appropriate folder
+        ee_token = os.environ["EARTHENGINE_TOKEN"]
+        credential_folder_path = Path.home() / ".config" / "earthengine"
+        credential_folder_path.mkdir(parents=True, exist_ok=True)
+        credential_file_path = credential_folder_path / "credentials"
+        credential_file_path.write_text(ee_token)
+
+    # if the user is in local development the authentication should
+    # already be available
+    ee.Initialize(http_transport=httplib2.Http())
