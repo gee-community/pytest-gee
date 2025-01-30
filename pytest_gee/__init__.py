3e092dd7f3370491e78248773008f6a4054a1714
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2025-01-30 09:40:57 +0000

    bump: version 0.7.0 → 0.7.1

--- pytest_gee/__init__.py
+++ pytest_gee/__init__.py
@@ -13,7 +13,7 @@ import httplib2
 from deprecated.sphinx import deprecated
 from ee.cli.utils import wait_for_task
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 __author__ = "Pierrick Rambaud"
 __email__ = "pierrick.rambaud49@gmail.com"
 
