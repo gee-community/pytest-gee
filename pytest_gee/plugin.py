25e08fe14befc2c3823739902bd517803c9c0442
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2025-10-15 13:50:49 +0000

    fix: try to use the pre-build value

--- pytest_gee/plugin.py
+++ pytest_gee/plugin.py
@@ -2,7 +2,6 @@
 
 from __future__ import annotations
 
-import os
 import uuid
 from pathlib import Path
 
@@ -27,13 +26,13 @@ def gee_folder_root():
     """Link to the root folder of the connected account."""
     # The credential information cannot be reached from
     # the ee API as reported in https://issuetracker.google.com/issues/325020447
-    # project_id = get_state().cloud_api_user_project
-    project_id = os.environ.get("EARTHENGINE_PROJECT", get_state().cloud_api_user_project)
-    if project_id is None:
-        raise ValueError(
-            "The project name cannot be detected."
-            "Please set the EARTHENGINE_PROJECT environment variable."
-        )
+    project_id = get_state().cloud_api_user_project
+    # project_id = os.environ.get("EARTHENGINE_PROJECT", get_state().cloud_api_user_project)
+    # if project_id is None:
+    #     raise ValueError(
+    #         "The project name cannot be detected."
+    #         "Please set the EARTHENGINE_PROJECT environment variable."
+    #     )
     return Path(f"projects/{project_id}/assets")
 
 
